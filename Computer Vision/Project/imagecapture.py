0b1e894e7545192471ba80559f3c448f7a427941
Upayan Mazumder <upayanm3@gmail.com>
2025-02-05 18:21:31 +0530

    Refactor image capture functionality into a new script

--- Computer Vision/Project/imagecapture.py
+++ Computer Vision/Project/imagecapture.py
@@ -4,7 +4,7 @@ import numpy
 import urllib.request
 haar_file = 'haarcascade_frontalface_default.xml'
 datasets = 'images'  
-sub_data = 'Your_Name'     
+sub_data = 'Upayan'     
 
 path = os.path.join(datasets, sub_data)
 if not os.path.isdir(path):
@@ -12,15 +12,15 @@ if not os.path.isdir(path):
 
 (width, height) = (130, 100)
 face_cascade = cv2.CascadeClassifier(haar_file)
-# webcam = cv2.VideoCapture(0)
-url='http://192.168.202.230:8080/shot.jpg'
+webcam = cv2.VideoCapture(0)
+# url='http://192.168.202.230:8080/shot.jpg'
 count = 1
 while count < 601:
     print(count)
-    # (_, im) = webcam.read()
-    imgPath = urllib.request.urlopen(url)
-    imgNp = numpy.array(bytearray(imgPath.read()), dtype=numpy.uint8)
-    im = cv2.imdecode(imgNp, -1)
+    (_, im) = webcam.read()
+    # imgPath = urllib.request.urlopen(url)
+    # imgNp = numpy.array(bytearray(imgPath.read()), dtype=numpy.uint8)
+    # im = cv2.imdecode(imgNp, -1)
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
     for (x,y,w,h) in faces:
